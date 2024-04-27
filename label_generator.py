3c8c36c450e08da076be6a97ebf9e5c4b01709b9
Meg Horikawa <meg.horikawa@gmail.com>
2024-04-27 17:39:06 +0200

    adding annotations

--- label_generator.py
+++ label_generator.py
@@ -4,13 +4,13 @@ date='4/27/2024'
 
 
 df = pd.read_csv('tatoeba_en_ja.csv')
-filtered_row = df.loc[:200]
+filtered_row = df.loc[45:48]
 
 labels = []
 gloss=[]
 
 for index, row in filtered_row.iterrows():
-    label = f"# ::id ja:{i} ::date {date} \n# ::snt {row['ja']}\n# ::gls {row['en']}\n \n"
+    label = f"# ::id ja:{row} ::date {date} \n# ::snt {row['ja']}\n# ::gls {row['en']}\n \n"
     gloss.append(f"{row['en']}\n")
     labels.append(label)
 
