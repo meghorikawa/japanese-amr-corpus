f59322ef8d592806987735679e65dc10d4a6533d
Meg Horikawa <meg.horikawa@gmail.com>
2024-05-10 15:29:03 +0200

    adding annotations

--- label_generator.py
+++ label_generator.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-date='5/05/2024'
+date='5/10/2024'
 
 
 df = pd.read_csv('tatoeba_en_ja.csv')
-filtered_row = df.loc[81:90]
+filtered_row = df.loc[91:99]
 
 labels = []
 gloss=[]
